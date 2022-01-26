8262552747a8f675943b51b2e019edda202ebed7
schollar <cschollar22@gmail.com>
2022-01-25 19:59:48 -0700

    Started project skeleton

---
+++ app.py
@@ -0,0 +1,10 @@
+import addition
+import division
+import multiplication
+import subtraction
+
+print('Please select from the following options')
+print('1.Addition')
+print('2.Subtraction')
+print('3.Multiplication')
+print('4.Division')
---
+++ subtraction.py
@@ -0,0 +1,2 @@
+def subtract_numbers(num1, num2):
+    print(num1 - num2)
